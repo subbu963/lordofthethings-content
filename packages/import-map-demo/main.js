b09c1581a745038b3bcd0a6bde9c727a80e15ce2
Aditya Subramanyam <aditya.katakamediga@walmart.com>
2023-02-10 16:04:12 +0530

    added import map demo

---
+++ packages/import-map-demo/main.js
@@ -0,0 +1,3 @@
+import _ from 'lodash';
+
+document.getElementById('main').innerText = `main script is using lodash@${_.VERSION}`;
---
+++ packages/import-map-demo/not-main.js
@@ -0,0 +1,3 @@
+import _ from 'lodash';
+
+document.getElementById('not-main').innerText = `not-main script is using lodash@${_.VERSION}`;
